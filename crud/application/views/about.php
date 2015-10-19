1a7aa52f37d40cb294c9108a79114f82b51150dc
elraghifary <melraghifaryr@gmail.com>
2015-10-19 14:25:26 +0700

    Create about.php

---
+++ crud/application/views/about.php
@@ -0,0 +1,33 @@
+<head><?php echo $map['js']; ?></head>
+
+<div class="container">
+	<h1>RSIA Humana Prima</h1>
+	<dl>
+		<dt>VISI RSIA HUMANA PRIMA</dt>
+		<dd>Menurunkan angka kesakitan dan kematian maternal dan perinatal melalui PELAYANAN MEDIK PRIMA.</dd>
+		<br>
+		<dt>MISI RSIA HUMANA PRIMA</dt>
+		<dd><ul>
+			<li>Menurunkan angka kesakitan dan kematian maternal dan perinatal.</li>
+			<li>Memberikan pelayanan kehamilan persalinan secara PROFESIONAL dan PARIPURNA.</li>
+			<li>Meningkatkan kualitas sumber daya di bidang pelayanan Ibu dan Anak.</li>
+			<li>Pemeliharaan dan peningkatan Kualitas Kesehatan Ibu hamil dan Anak.</li>
+			<li>Mengoptimalkan tumbuh kembang anak.</li>
+			<li>Penyelenggaraan pelayanan yang BERMUTU, MERATA dan TERJANGKAU.</li>
+			<li>Meningkatkan pemberian ASI eksklusif dan Inisiasi dini menyusui pada ibu baru melahirkan.</li>		
+			<li>Pemeliharaan dan peningkatan kesehatan individu, keluarga dan masyarakat beserta lingkungannya.</li>
+		</ul></dd>
+	</dt>
+
+	<address>
+	  <strong>Address</strong><br>
+	  <div class="row"><div class="col-xs-1 col-md-5"><?php echo $map['html']; ?></div></div>
+	  Jalan Rancabolang No. 21<br>
+	  Bandung, Indonesia 40286<br>
+	  <abbr title="Phone">P:</abbr> (022) 7563701 - 7563543
+	</address>
+	<address>
+	  <strong>Email</strong><br>
+	  <a href="mailto:#">humanaprima@gmail.com</a>
+	</address>
+</div>
